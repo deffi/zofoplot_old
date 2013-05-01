3b6c074567834381e5c2868044af01247cd3a862
Martin Herrmann <martin@famherrmann.de>
2013-05-01 15:15:34 +0200

    Extended generalized setter (allow attribute creation from single argument)

--- lib/zofo_plot/container.rb
+++ lib/zofo_plot/container.rb
@@ -8,38 +8,24 @@
 # Calling a block in the context of the attribute: 
 #   line.color { red 255; green 0; blue 255 }
 #
-# Setting a value by regular or generalized setter:
+# Assigning a value by regular or generalized setter:
 #   line.color my_color
 #   line.color=my_color
 #
-# Assigning a converted value by generalized setter:
+# Creating a converted value by generalized setter:
 #   line.color "#ff00ff"
 #   line.color [255, 0, 255]
 #   line.color 255, 0, 255
-#   line.color hash, where hash = { red => 255, green => 0, blue => 255 }
-#     Note that we can't specify a hash literal, this would clash with a block 
 #
-# Do we want to allow assigning a converted value by regular setter?
+# We do not allow creating a converted value by regular setter (e. g.
+# line.color=1, 2, 3), for two reasons:
 #   1. this does not preserve assignment semantics
-#   2. check this - if left is sometimes the same as right and sometimes not,
-#      it's probably not a good idea:
-#      class Foo
-#        def x(*args) ; p args; end
-#        def x=(*args); p args; end
-#      end
-#
-#      foo=Foo.new
-#      foo.x [1,2,3]; foo.x=[1,2,3]
-#      foo.x 1,2,3  ; foo.x=1,2,3
-#      foo.x arr    ; foo.x=arr
- 
-
-
-# Old: We don't allow assigning a converted value by regular setter, for two reasons:
-#   1. `line.color=255, 0, 255` passes an array to the setter: [255, 0, 255]
-#        1. line.color [a,b,c] could mean something different from line.color=[a,b,c]
-#        2. 
-#   2. We want to preserve setter semantics.
+#   2. a method with a name ending in `=` receives varargs wrapped in an array
+#      (`args==[[1, 2, 3]]`) instead of individual values (`args==[1, 2, 3]`),
+#      as if it had been called with an array as a parameter:
+#      `foo=1,2,3` is the same as    `foo=[1,2,3]`
+#      `foo 1,2,3` is different from `foo [1,2,3]`
+#      Additionally, we cannot call foo=(1,2,3)
 
 module ZofoPlot
 	# Provides a generalized setter/getter.
@@ -60,8 +46,8 @@ module ZofoPlot
 	#     class Document
 	#       include Container
 	#       zofo_attributes :title, :body
-    #
-	#       def initialzie
+  #
+	#       def initialize
 	#         @title=Text.new
 	#         @body =Text.new
 	#       end
@@ -85,22 +71,43 @@ module ZofoPlot
 		end
 
 		module ClassMethods
+		  def zofo_attribute(name, klass)
+		    zofo_attributes name
+		    @@zofo_attributes ||= {}
+		    @@zofo_attributes[name]=klass
+		    puts "Added attribute #{name}, now we have #{@@zofo_attributes}"
+		  end
+		  
+		  # FIXME allow specifying a class (like for zofo_attribute) using a hash
 			def zofo_attributes(*attributes)
 				attr_writer *attributes
 
 				# For each attribute, define the generalezed setter. The
 				# generalized setter accepts a value or a proc and always returns
 				# the (new) value.
-				attributes.each { |a|
-					define_method(a) { |value=(value_omitted=true), &proc|
-						variable="@#{a}"
+				attributes.each { |attribute|
+					define_method(attribute) { |value=(value_omitted=true), &proc|
+						variable_name="@#{attribute}"
 
 						unless value_omitted
-							#puts "Assigning a #{value.class} to #{self.class}'s #{a}"
-							instance_variable_set(variable, value)
+							#puts "Assigning a #{value.class} to #{self.class}'s #{attribute}"
+						  # Retrieve the expected class
+						  expected_class=@@zofo_attributes[attribute]
+						  if expected_class.nil?
+                # We don't expect a specific class - assign the value 
+                instance_variable_set(variable_name, value)
+						  elsif value.is_a?(expected_class)
+						    # The value matches the expected class - assign it 
+						    instance_variable_set(variable_name, value)
+						  else
+						    # The value does not match the expected class - try to create
+						    # a new instance of the expected class
+                puts "Expected #{expected_class}, got #{value.class} - try to create it"
+                instance_variable_set(variable_name, expected_class.new(value))
+						  end
 						end
 
-						value=instance_variable_get(variable)
+						value=instance_variable_get(variable_name)
 
 						if proc
 							#puts "Executing #{proc} in the context of the #{value.class}"
--- test/container_test.rb
+++ test/container_test.rb
@@ -10,12 +10,27 @@ module ZofoPlot
 		    include ZofoPlot::Container
 		    
 		    zofo_attributes :red, :green, :blue
+		    
+		    def initialize(*args)
+		      if args.size==3
+		        @red, @green, @blue = args
+		      elsif args.size==1 && args[0].is_a?(String) && args[0] =~ /^ [#]? (\h{2}) (\h{2}) (\h{2}) $ /x
+            @red  =$1.to_i(16)
+            @green=$2.to_i(16)
+            @blue =$3.to_i(16)
+          end
+		    end
+		    
+		    def to_a
+		      [@red, @green, @blue]
+		    end
 		end
 		
 		class MyLine
 		    include ZofoPlot::Container
 		    
-		    zofo_attributes :width, :name, :color
+		    zofo_attribute :color, MyColor
+		    zofo_attributes :width, :name
 		end
 		
 		def test_attributes
@@ -34,8 +49,19 @@ module ZofoPlot
 
 			# Modify a value - regular setter, regular getter, block
 			my_line.name="hello"      ; assert_equal "hello", my_line.name
-            my_line.name.upcase!      ; assert_equal "HELLO", my_line.name
+      my_line.name.upcase!      ; assert_equal "HELLO", my_line.name
 			my_line.name { downcase! }; assert_equal "hello", my_line.name
+			  
+      # Assign a value
+      my_line.color MyColor.new(0, 127, 255)
+      assert_equal MyColor, my_line.color.class
+      assert_equal [0, 127, 255], my_line.color.to_a
+      
+      # Generalized create a value
+      my_line.color "#007fff"
+      assert_equal MyColor, my_line.color.class
+      assert_equal [0, 127, 255], my_line.color.to_a
+
 		end
 	end
 end
