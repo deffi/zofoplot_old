3e38826e9b7c015b41deef4dcff87cf8af7b4fe9
Martin Herrmann <martin@famherrmann.de>
2013-05-02 09:18:14 +0200

    Indent

--- example.rb
+++ example.rb
@@ -5,7 +5,6 @@ require 'pathname'
 
 require_relative 'lib/zofo_plot'
 
-
 degree=Math::PI/180
 
 include ZofoPlot
@@ -14,49 +13,49 @@ quadratic_points=(0..5).map { |x| DefaultPoint.new(x, x**2) }
 quadratic_points[4].y_range=[13, 17]
 linear_points=(0..5).map { |x| DefaultPoint.new(x, 5*x) }
 hexagon_points=(0..6).map { |i|
-	alpha=i/6.0*2*Math::PI
-	DefaultPoint.new(2*Math::cos(alpha)+2, 2*Math::sin(alpha)+2)
+    alpha=i/6.0*2*Math::PI
+    DefaultPoint.new(2*Math::cos(alpha)+2, 2*Math::sin(alpha)+2)
 }
 
 linear_data_set=DataSet.create {
-	points linear_points
-	line_style  { color "#7fff00"; width 2  }
-	point_style { color "#7f00ff"; shape :filled_triangle_down }
-	title "Linear"
+    points linear_points
+    line_style  { color "#7fff00"; width 2  }
+    point_style { color "#7f00ff"; shape :filled_triangle_down }
+    title "Linear"
 }
 
 quadratic_data_set=DataSet.create {
-	points      quadratic_points
-	line_style  { color "#ff7f00"; width 1  }
-	point_style { color "#7f00ff"; shape :filled_triangle_up }
+    points      quadratic_points
+    line_style  { color "#ff7f00"; width 1  }
+    point_style { color "#7f00ff"; shape :filled_triangle_up }
 }
 
 hexagon_data_set=DataSet.create {
-	points hexagon_points
-	line_style { color "#007fff" }
-	point_style { shape :none }
+    points hexagon_points
+    line_style { color "#007fff" }
+    point_style { shape :none }
 }
 
 lines_chart=Chart.create {
-	title "Some chart"
-	x_axis { range [-0.5, 5.5] ; label "All right"; tics {
-		visible true
-	} }
-	y_axis { range [0, 35]     ; label "Updog"; tics {
-		visible true
-	} }
-	data_sets << quadratic_data_set
-	data_sets << linear_data_set
+    title "Some chart"
+    x_axis { range [-0.5, 5.5] ; label "All right"; tics {
+            visible true
+        } }
+    y_axis { range [0, 35]     ; label "Updog"; tics {
+            visible true
+        } }
+    data_sets << quadratic_data_set
+    data_sets << linear_data_set
 
-	data_sets << DataSet.create {
-		points [[1,5], [3,5], [5,5]].map { |x,y| DefaultPoint.new(x,y) }
-		point_style { shape :open_diamond }
-	}
+    data_sets << DataSet.create {
+        points [[1,5], [3,5], [5,5]].map { |x,y| DefaultPoint.new(x,y) }
+        point_style { shape :open_diamond }
+    }
 }
 
 hexagon_chart=Chart.create {
-	y_axis { logarithmic }
-	data_sets << hexagon_data_set
+    y_axis { logarithmic }
+    data_sets << hexagon_data_set
 }
 
 #chart=Chart.create {
@@ -70,37 +69,35 @@ hexagon_chart=Chart.create {
 #}
 
 multi_chart=MultiChart.create {
-	charts << lines_chart << hexagon_chart
+    charts << lines_chart << hexagon_chart
 }
 
-
-
 outputs=[
-	[lines_chart  , Pathname.pwd+"example_plots"+"zofo_lines.png"     , [1024, 768]],
-#	[hexagon_chart, Pathname.pwd+"example_plots"+"zofo_hexagon.png"   , [1024, 768]],
-	[multi_chart  , Pathname.pwd+"example_plots"+"png_small.png"      , [1024, 768]],
-#	[multi_chart  , Pathname.pwd+"example_plots"+"png_large.png"      , [2048, 1536]],
-#	[multi_chart  , Pathname.pwd+"example_plots"+"svg_small.svg"      , [1024, 768]],
-#	[multi_chart  , Pathname.pwd+"example_plots"+"svg_large.svg"      , [2048, 1536]],
-#	[multi_chart  , Pathname.pwd+"example_plots"+"emf_small.emf"      , [1024, 768]],
-#	[multi_chart  , Pathname.pwd+"example_plots"+"emf_large.emf"      , [2048, 1536]],
-#	[multi_chart  , Pathname.pwd+"example_plots"+"pdf_small.pdf"      , [8, 6]],
-	[multi_chart  , Pathname.pwd+"example_plots"+"pdf_large.pdf"      , [16, 12]]
-	]
+    [lines_chart  , Pathname.pwd+"example_plots"+"zofo_lines.png"     , [1024, 768]],
+    #	[hexagon_chart, Pathname.pwd+"example_plots"+"zofo_hexagon.png"   , [1024, 768]],
+    [multi_chart  , Pathname.pwd+"example_plots"+"png_small.png"      , [1024, 768]],
+    #	[multi_chart  , Pathname.pwd+"example_plots"+"png_large.png"      , [2048, 1536]],
+    #	[multi_chart  , Pathname.pwd+"example_plots"+"svg_small.svg"      , [1024, 768]],
+    #	[multi_chart  , Pathname.pwd+"example_plots"+"svg_large.svg"      , [2048, 1536]],
+    #	[multi_chart  , Pathname.pwd+"example_plots"+"emf_small.emf"      , [1024, 768]],
+    #	[multi_chart  , Pathname.pwd+"example_plots"+"emf_large.emf"      , [2048, 1536]],
+    #	[multi_chart  , Pathname.pwd+"example_plots"+"pdf_small.pdf"      , [8, 6]],
+    [multi_chart  , Pathname.pwd+"example_plots"+"pdf_large.pdf"      , [16, 12]]
+]
 
 Engine.new { |engine|
-	outputs.each { |chart, file, size|
-		file.unlink if file.exist?
-		file.parent.mkpath
-		engine.render chart, file, size
-	}
+    outputs.each { |chart, file, size|
+        file.unlink if file.exist?
+        file.parent.mkpath
+        engine.render chart, file, size
+    }
 }
 
 #file=Pathname.pwd+"example_plots"+"zofo_lines.png"
 file=Pathname.pwd+"example_plots"+"png_small.png"
 if file.exist?
-	Kernel.system "start #{file}"
+    Kernel.system "start #{file}"
 else
-	puts "#{file} does not exist."
+    puts "#{file} does not exist."
 end
 
