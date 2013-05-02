d1598e0397f37e315b2a9169339fd08c0a97758c
Martin Herrmann <martin@famherrmann.de>
2013-05-02 09:06:49 +0200

    Update documentation

--- lib/zofo_plot/container.rb
+++ lib/zofo_plot/container.rb
@@ -1,78 +1,75 @@
-# FIXME current: proper documentation for this
-# to something other than simply a reference.
-#
-# Example:
-#   color          is a zofo_attribute  of line
-#   red/green/blue are  zofo_attributes of color
-#
-# Calling a block in the context of the attribute: 
-#   line.color { red 255; green 0; blue 255 }
-#
-# Assigning a value by regular or generalized setter:
-#   line.color my_color
-#   line.color=my_color
-#
-# Creating a converted value by generalized setter:
-#   line.color "#ff00ff"
-#   line.color [255, 0, 255]
-#   line.color 255, 0, 255
-#
-# We do not allow creating a converted value by regular setter (e. g.
-# line.color=1, 2, 3), for two reasons:
-#   1. this does not preserve assignment semantics
-#   2. a method with a name ending in `=` receives varargs wrapped in an array
-#      (`args==[[1, 2, 3]]`) instead of individual values (`args==[1, 2, 3]`),
-#      as if it had been called with an array as a parameter:
-#      `foo=1,2,3` is the same as    `foo=[1,2,3]`
-#      `foo 1,2,3` is different from `foo [1,2,3]`
-#      Additionally, we cannot call foo=(1,2,3)
-
 module ZofoPlot
-	# Provides a generalized setter/getter.
+	# Provides a generalized accessor
 	#
-	# To define the generalized setter, use the class method zofo_attributes.
-	# This method works like attr_accessor, but the getter can also be used
-	# as a setter by passing the value to be set as an argument.
-	# Additionally, a block can be passed to the getter, which will be
-	# executed in the context of the attribute. This is particularly useful
-	# if the attribute is a Container itself.
-	#
-	# Example:
-	#     class Text
-	#       include Container
-	#       zofo_attributes :string, :size
-	#     end
-	#
-	#     class Document
-	#       include Container
-	#       zofo_attributes :title, :body
-  #
-	#       def initialize
-	#         @title=Text.new
-	#         @body =Text.new
-	#       end
-	#     end
-	#
-	#     my_text=Text.new
-	#     my_text.string = "Hello"  # Sets and returns the value (like an attr_writer)
-	#     my_text.size     24       # Sets and returns the value (like an attr_writer)
-	#     my_text.size              # Returns the value          (like an attr_reader)
-	#     
-	#     my_document=Document.new
-	#     my_document.title = my_text
-	#     my_document.body {        # Executes the block in the context of the value
-	#       string "...world!"
-	#       size 12
-	#       size                    # Returns the value
-	#     }
+	# To define the generalized accessor, use the class method zofo_attributes.
+	# This method works like attr_accessor, but the methods it creates are more
+    # versatile.
+    #
+    # As an example, let's assume we have two containers:
+    #     class Color
+    #       include Container
+    #       zofo_attributes :red, :green, :blue
+    #     end
+    #
+    #     class Line
+    #       include Container
+    #       zofo_attributes :width, :color
+    #     end
+    #
+    #     my_color=Color.new
+    #     my_line =Line.new
+    #
+    # The attribute can be accessed in the following ways:
+    #   * regular setter: sets and returns the (new) value. This is identical to
+    #     the write accessor created with attr_writer:  
+    #         my_color.red = 127
+    #   * regular getter: returns the current value. This is identical to the
+    #     read accessor created with attr_reader:
+    #         my_color.red
+    #   * generalized setter: allows setting the value with a different syntsx:
+    #         my_text.red 255
+    #   * block accessor: executes a block in he context of the value. This is
+    #     particularly useful if the value is itself a Container:
+    #         my_line.color {
+    #           red 0
+    #           green 127 
+    #           blue 255
+    #         }
+    #
+    # The attribute can also be associated with a class. This allows the
+    # following additional access method:
+    #   * creator: when the value passed to the generalized setter does not
+    #     match the expected class, or if multiple values are passed to the
+    #     generalized setter, a new instance of the associated class is created,
+    #     using the value(s) passed as constructor arguments. These examples
+    #     assume that the Color class has a constructor that can handle the
+    #     respective arguments:
+    #         my_line.color "#007fff"
+    #         my_line.color [0, 127, 255]
+    #         my_line.color 0, 127, 255
+    #
+    # Note that we do not allow creating a converted value by regular setter
+    # (e. g. `line.color="#007fff"` or `line.color=1,2,3`), for two reasons:
+    #   * this does not preserve assignment semantics
+    #   * it seems that a method with a name ending in `=` receives varargs
+    #     wrapped in an array instead of individual values, as if it had been
+    #     called with an array as a parameter:
+    #         c.x [1,2,3]   => args==[[1, 2, 3]]
+    #         c.x=[1,2,3]   => args==[[1, 2, 3]]
+    #         c.x  1,2,3    => args== [1, 2, 3]
+    #         c.x= 1,2,3    => args==[[1, 2, 3]], this is inconsistent
+    #     Additionally, we cannot call a method ending in `=` with the arguments
+    #     in parenteses (`foo=(1,2,3)` does not work).
+    
 	module Container
 		def self.included(klass)
 			klass.extend(ClassMethods)
 
-			# Initialize the @zofo_attributes instance variable of the class (!) to an
-			# empty hash. This variable is used to store additional information about
-			# the attributes. The zofo_attributes class method, which is used for
-			# defining attributes, also acts as a getter for this variable. 
+			# Initialize the @zofo_attributes instance variable of the class (!)
+			# to an empty hash. This variable is used to store additional
+			# information about the attributes. The zofo_attributes class
+			# method, which is used for defining attributes, also acts as a
+			# getter for this variable. 
 			klass.instance_variable_set :@zofo_attributes, Hash.new
 		end
 
