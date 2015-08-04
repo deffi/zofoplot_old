bce222240228aa2c7ebf8f0336dd97615372bb11
Martin Herrmann <martin@famherrmann.de>
2015-08-04 21:22:18 +0200

    Remove obsolete stuff

--- old/test/all_tests.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-Dir["#{File.dirname(File.expand_path(__FILE__))}/**/*_test.rb"].sort.each { |entry|
-    require entry
-}
-
-# at_axit, the AutoRunner will be invoked
-
--- zofoplot/style_element.py
+++
@@ -1,6 +0,0 @@
-class StyleElement:
-    def __init__(self, scope, key, value):
-        self.key = key
-        self.value = value
-        
-        pass
